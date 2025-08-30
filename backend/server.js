23894df93d09bcaf4bdbba6cba83a77df59194d3
krishna-85 <vaibhavchauhan658@gmail.com>
2025-08-30 17:13:46 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,11 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://animal-rescue-ui.onrender.com"], // frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true, // agar cookies/auth use ho rahi ho
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const storage = multer.diskStorage({});
